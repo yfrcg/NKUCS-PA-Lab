1b7f6f1798df903117a0c576bb18af21489b58b6
tracer-ics2017 <tracer@njuics.org>
2026-04-04 07:10:19 -0400

    >  compile 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  07:10:19 up  5:48,  1 user,  load average: 0.02, 0.04, 0.00 1795fb0efb152511a94f49c6fe637929dde25a17

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -13,8 +13,9 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  TODO();
-
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
   print_asm_template2(xor);
 }
 
