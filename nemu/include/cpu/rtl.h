ac4933b7908fde00a336bab17725978505c1a539
tracer-ics2017 <tracer@njuics.org>
2026-04-04 06:38:42 -0400

    >  compile 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  06:38:42 up  5:17,  1 user,  load average: 0.00, 0.01, 0.00 e05ac51e1f0279074f116c2cdfdb4b0ca7628293

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -126,34 +126,39 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  TODO();
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  *dest = ~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  int32_t temp = (int32_t)*src1;
+  int shift = (4 - width) * 8;
+  *dest = (temp << shift) >> shift;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
+  cpu.esp -= 4 ;
   // M[esp] <- src1
-  TODO();
+  vaddr_write(cpu.esp, 4, *src1);
+  
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
+  *dest = vaddr_read(cpu.esp, 4);
   // esp <- esp + 4
-  TODO();
+  cpu.esp += 4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  *dest = (*src1 == 0 ? 1 : 0);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
