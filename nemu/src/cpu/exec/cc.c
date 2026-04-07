4553a81298f5a8d8b2c992a69bb36b0b0c11eb9f
tracer-ics2017 <tracer@njuics.org>
2026-04-07 02:06:12 -0400

    >  compile 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  02:06:12 up 44 min,  1 user,  load average: 0.23, 0.08, 0.10 c6a93f2e34146c652ab5f3354394a807b8f5896f

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,15 +14,13 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      rtl_li(dest, cpu.ZF);
-      break;
+    case CC_O:  rtl_li(dest, cpu.OF); break;
+    case CC_B:  rtl_li(dest, cpu.CF); break;
+    case CC_E:  rtl_li(dest, cpu.ZF); break;
+    case CC_BE: rtl_li(dest, (cpu.CF || cpu.ZF)); break;
+    case CC_S:  rtl_li(dest, cpu.SF); break;
+    case CC_L:  rtl_li(dest, (cpu.SF != cpu.OF)); break;
+    case CC_LE: rtl_li(dest, (cpu.ZF || (cpu.SF != cpu.OF))); break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
