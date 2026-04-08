71258561dd6cec7a40865a193ff69267bce5e0e2
tracer-ics2017 <tracer@njuics.org>
2026-04-08 04:41:18 -0400

    >  compile 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  04:41:18 up  1:08,  1 user,  load average: 0.11, 0.03, 0.03 6aded29ec5a6c4d4d482fb6af42150599de16c67

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -2,6 +2,10 @@
 #include <x86.h>
 
 #define RTC_PORT 0x48   // Note that this is not standard
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
+#define I8042_STATUS_HASKEY_MASK 0x1
+
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -22,9 +26,19 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  for (int row = 0; row < h; row ++) {
+    int dst_y = y + row;
+    if (dst_y < 0 || dst_y >= _screen.height) {
+      continue;
+    }
+
+    for (int col = 0; col < w; col ++) {
+      int dst_x = x + col;
+      if (dst_x < 0 || dst_x >= _screen.width) {
+        continue;
+      }
+      fb[dst_y * _screen.width + dst_x] = pixels[row * w + col];
+    }
   }
 }
 
@@ -32,5 +46,8 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  return _KEY_NONE;
+  if ((inb(I8042_STATUS_PORT) & I8042_STATUS_HASKEY_MASK) == 0) {
+    return _KEY_NONE;
+  }
+  return inl(I8042_DATA_PORT);
 }
