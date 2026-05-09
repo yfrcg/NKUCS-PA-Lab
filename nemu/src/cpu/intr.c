571bc3884399e1232072f26cd579ca0f6c7e6563
tracer-ics2017 <tracer@njuics.org>
2026-05-09 03:26:39 -0400

    >  compile 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  03:26:39 up  1:46,  1 user,  load average: 0.01, 0.04, 0.07 f05ee578c51d60a0843d932518dcd2f31b329690

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -1,13 +1,19 @@
 #include "cpu/exec.h"
-#include "memory/memory.h"
+#include "memory/mmu.h"
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   vaddr_t gate_addr = cpu.idtr.base + NO * 8;
+
+  Assert(cpu.idtr.base != 0, "IDTR is not initialized");
+  Assert(NO * 8 + 7 <= cpu.idtr.limit,
+      "Interrupt number 0x%x is out of IDT limit 0x%x", NO, cpu.idtr.limit);
 
   uint32_t gate_low = vaddr_read(gate_addr, 4);
   uint32_t gate_high = vaddr_read(gate_addr + 4, 4);
 
   vaddr_t target = (gate_low & 0xffff) | (gate_high & 0xffff0000);
+
+  Assert(target != 0, "IDT gate target is 0, NO = 0x%x", NO);
 
   cpu.esp -= 4;
   vaddr_write(cpu.esp, 4, cpu.eflags);
