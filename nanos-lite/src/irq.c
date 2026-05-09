039f3f17a7a3158b1926ccbf55a2c9ba9e125955
tracer-ics2017 <tracer@njuics.org>
2026-05-09 04:12:07 -0400

    >  run 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  04:12:07 up  2:24,  1 user,  load average: 0.00, 0.00, 0.00 c3e82b98b5f2c3668b32b197755dae07ee1959dc

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ _RegSet* do_syscall(_RegSet *r);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
-    case 3:   // 当前日志里 _EVENT_SYSCALL 的实际编号就是 3
+    case _EVENT_SYSCALL:
       return do_syscall(r);
 
     default:
