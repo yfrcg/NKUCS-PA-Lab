f66b1c35e6e4561bf34ee0f54c2ce0803d2e5d43
tracer-ics2017 <tracer@njuics.org>
2026-05-09 04:30:04 -0400

    >  run 2411080 yfrcg Linux yfrcg-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  04:30:04 up  2:42,  1 user,  load average: 0.61, 0.19, 0.05 dc4082affa1fa791ef483c8c62fa237aca57ea0

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,23 +4,118 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin (note that this is not the actual stdin)", 0, 0},
-  {"stdout (note that this is not the actual stdout)", 0, 0},
-  {"stderr (note that this is not the actual stderr)", 0, 0},
-  [FD_FB] = {"/dev/fb", 0, 0},
-  [FD_EVENTS] = {"/dev/events", 0, 0},
-  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
+  {"stdin (note that this is not the actual stdin)", 0, 0, 0},
+  {"stdout (note that this is not the actual stdout)", 0, 0, 0},
+  {"stderr (note that this is not the actual stderr)", 0, 0, 0},
+  [FD_FB] = {"/dev/fb", 0, 0, 0},
+  [FD_EVENTS] = {"/dev/events", 0, 0, 0},
+  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, 0},
 #include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+size_t fs_filesz(int fd) {
+  assert(fd >= 0 && fd < NR_FILES);
+  return file_table[fd].size;
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < NR_FILES; i++) {
+    if (strcmp(file_table[i].name, pathname) == 0) {
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+
+  panic("fs_open: file not found: %s", pathname);
+  return -1;
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  if (fd < FD_NORMAL) {
+    return 0;
+  }
+
+  size_t remain = file_table[fd].size - file_table[fd].open_offset;
+  if (len > remain) {
+    len = remain;
+  }
+
+  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+
+  return len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  if (fd == FD_STDOUT || fd == FD_STDERR) {
+    const char *s = (const char *)buf;
+    for (size_t i = 0; i < len; i++) {
+      _putc(s[i]);
+    }
+    return len;
+  }
+
+  if (fd < FD_NORMAL) {
+    return 0;
+  }
+
+  size_t remain = file_table[fd].size - file_table[fd].open_offset;
+  if (len > remain) {
+    len = remain;
+  }
+
+  ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+
+  return len;
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  off_t new_offset = 0;
+
+  switch (whence) {
+    case SEEK_SET:
+      new_offset = offset;
+      break;
+    case SEEK_CUR:
+      new_offset = file_table[fd].open_offset + offset;
+      break;
+    case SEEK_END:
+      new_offset = file_table[fd].size + offset;
+      break;
+    default:
+      panic("fs_lseek: invalid whence = %d", whence);
+  }
+
+  assert(new_offset >= 0 && new_offset <= file_table[fd].size);
+  file_table[fd].open_offset = new_offset;
+
+  return new_offset;
+}
+
+int fs_close(int fd) {
+  assert(fd >= 0 && fd < NR_FILES);
+  return 0;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,12 @@
 #include "common.h"
 #include "syscall.h"
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
 
@@ -19,20 +25,27 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
 
     case SYS_write:
-      if (a[1] == 1 || a[1] == 2) {
-        char *buf = (char *)a[2];
-        size_t len = a[3];
-        for (size_t i = 0; i < len; i++) {
-          _putc(buf[i]);
-        }
-        r->eax = len;
-      } else {
-        r->eax = -1;
-      }
+      r->eax = fs_write(a[1], (void *)a[2], a[3]);
       break;
 
     case SYS_brk:
       r->eax = 0;
+      break;
+
+    case SYS_open:
+      r->eax = fs_open((const char *)a[1], a[2], a[3]);
+      break;
+
+    case SYS_read:
+      r->eax = fs_read(a[1], (void *)a[2], a[3]);
+      break;
+
+    case SYS_close:
+      r->eax = fs_close(a[1]);
+      break;
+
+    case SYS_lseek:
+      r->eax = fs_lseek(a[1], a[2], a[3]);
       break;
 
     default:
